2a63a4463ddc371ef81dc513561e5e47c166768a
untitled <ahmedsuleiman940@gmail.com>
2025-07-30 00:12:16 +0100

    clea slate

--- backend/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { loginUser, signup, verifyEmail } = require('../controllers/authController');
-
-router.post('/login', loginUser);
-router.post('/signup', signup);
-router.post('/verify', verifyEmail);
-
-module.exports = router;
