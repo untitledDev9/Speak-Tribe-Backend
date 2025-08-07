152807a5c264bfa6e1431b5279a892142c1e901a
untitled <ahmedsuleiman940@gmail.com>
2025-08-07 10:11:34 +0100

    added yoruba alphabet route

--- backend/routes/yoruba.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAlphabets } = require('../controllers/yorubaController');
-
-// GET all alphabets
-router.get('/alphabets', getAlphabets);
-
-module.exports = router;
